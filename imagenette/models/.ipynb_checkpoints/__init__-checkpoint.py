104d59425f89ee8e1612f34a635636749dd6cfc5
anke <altynanke@gmail.com>
2022-06-03 18:51:27 +0300

    removed cache

--- imagenette/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .Vgg import VGG16, VGG11, VGG_tiny
-from .resnet import Resnet18
-
-__all__ = ['VGG16', 'VGG11', 'VGG_tiny', 'Resnet18']
--- imagenette/utils/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from .data import generate_labels,  ImagenetteDataset, get_norm_stats, get_transforms, get_augmented_transforms
-from .pipeline import train, evaluate, train_pipeline
-
-__all__ = ['generate_labels', 'get_transforms', 'ImagenetteDataset', 'get_norm_stats',
-           'train', 'evaluate', 'train_pipeline', 'get_augmented_transforms']
