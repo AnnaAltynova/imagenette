5cf7a94fe4492f6accdba88963ac84e854c94806
anke <altynanke@gmail.com>
2022-06-03 18:18:40 +0300

    resnet fixed

--- imagenette/models/__init__.py
+++ imagenette/models/__init__.py
@@ -1,4 +1,4 @@
 from .Vgg import VGG16, VGG11, VGG_tiny
-from .resnet import resnet_brick, ResnetBlock, Resnet18
+from .resnet import Resnet18
 
-__all__ = ['VGG16', 'VGG11', 'VGG_tiny', 'resnet_brick', 'ResnetBlock', 'Resnet18']
+__all__ = ['VGG16', 'VGG11', 'VGG_tiny', 'Resnet18']
