1169a99ee43c8599b1cbf63a9f326a5e75069520
anke <altynanke@gmail.com>
2022-07-12 16:05:46 +0300

    added tests

--- imagenette/pixel_shuffle.py
+++ imagenette/pixel_shuffle.py
@@ -1,66 +1,87 @@
-import torch
+""" Layer implementing Pixel Shuffle for an arbitrary dimensionality.
+Shi et al. "`Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network_ 
+<https://arxiv.org/pdf/1609.05158.pdf>`"
+"""
+from torch import nn
 
-def pixel_shuffle(inputs, upscale_factor):
-    """Rearranges elements in a tensor of shape [*, C*r^N, D1, D2, ... DN] to a
-    tensor of shape [*, C, D1*r, D2*r, ... DN*r].
+
+
+class PixelShuffle(nn.Module):
+    """ Rearranges elements in a tensor of shape [B, C * r^N, D1, D2, ... DN] to a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
+    where r is an upscale factor.
     
-    See the paper:
-    `Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network`_
-    by Shi et. al (2016) for more details.
-    
-    Args:
-        inputs (Variable): Inputs
-        upscale_factor (int): factor to increase spatial resolution by
-    Examples:
-        >>> inputs = torch.randn(1, 64, 10, 10, 10))
-        >>> output = pixel_shuffle(inputs, 4)
-        >>> print(output.size())
-        torch.Size([1, 1, 40, 40, 40])
+    Parameters
+    ----------
+    upscale_factor : int
+        factor to increase spatial resolution by.
     """
-    batch_size, channels, *dims = inputs.size()
-    N = len(dims)
-    channels //= (upscale_factor ** N)
-    inputs_view = inputs.contiguous().view(batch_size, channels, *(upscale_factor for i in range(N)), *dims) # (bs, ch, r, ... r, d1, .... dN)
+    def __init__(self, upscale_factor):
+        super().__init__()
+        self.upscale_factor = upscale_factor
 
-    permute = [ None for i in range(2 * N)]
-    permute[::2] = range(N + 2, 2 * N + 2)
-    permute[1::2] = range(2, N + 2)
-    
-    # (bs, ch, r, ... r, d1, .... dN) -> (bs, ch, d1, r, d2, r, ... dN, r), idx: (0, 1, N + 2, 2, ... N + 1 + i, 2 + i, ... 2N + 1, N + 1)
-    shuffle_out = inputs_view.permute(0, 1, *permute).contiguous()  
-    out_dims = [dim * upscale_factor for dim in dims]
-    return shuffle_out.view(batch_size, channels, *out_dims)
+    def forward(self, x):
+        batch_size, channels, *dims = x.size()
+        ndims = len(dims)
+        channels //= (self.upscale_factor**ndims)
+        x = x.contiguous().view(                                                       # (B, C, r, r, H, W)
+            batch_size, channels, *(self.upscale_factor for i in range(ndims)), *dims
+            )
+
+        permute = [None] * (2 * ndims)
+        permute[::2] = range(ndims + 2, 2 * ndims + 2)
+        permute[1::2] = range(2, ndims + 2)
+        x = x.permute(0, 1, *permute).contiguous()                                     # (B, C, H, r, W, r)
+
+        out_dims = [dim * self.upscale_factor for dim in dims]
+        x = x.view(batch_size, channels, *out_dims).contiguous()                       # (B, C, H * r, W * r)
+        return x
+
+    def extra_repr(self):
+        """ Report upscale factor for a repr. """
+        info = f'upscale_factor={self.upscale_factor}'
+        return info
 
 
-def pixel_unshuffle(inputs, downscale_factor):
-    """Reverses `pixel_shuffle` operation by rearranging elements
-    in a tensor of shape [*, C, D1*r, D2*r, ... DN*r] to a tensor of shape
-    [*, C*r^N, D1, DN], where r is a downscale factor.
 
-    See the paper:
-    `Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network`_
-    by Shi et. al (2016) for more details.
-    
-    Args:
-        inputs (Variable): Inputs
-        downscale_factor (int): factor to decrease spatial resolution by
+class PixelUnshuffle(nn.Module):
+    """ Reverses `PixelShuffle` operation by rearranging elements in a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
+    to a tensor of shape [B, C * r^N, D1, DN], where r is a downscale factor.
+
+    Parameters
+    ----------
+    downscale_factor : int 
+        factor to decrease spatial resolution by.
     """
-    batch_size, channels, *dims = inputs.size()
-    N = len(dims)
+    def __init__(self, downscale_factor):
+        super().__init__()
+        self.downscale_factor = downscale_factor
+        
+    def forward(self, x):
+        batch_size, channels, *dims = x.size()
+        ndims = len(dims)
 
-    out_channels = channels * downscale_factor ** N
-    out_dims = [dim // downscale_factor for dim in dims]
+        x = x.contiguous()                                                             # (B, C, H * r, B * r)
 
-    reshape = [ None for i in range(2 * N)]
-    reshape[::2] = out_dims
-    reshape[1::2] = [downscale_factor for i in range(N)]
+        out_dims = [dim // self.downscale_factor for dim in dims]
+        reshape = [None] * (2 * ndims)
+        reshape[::2] = out_dims
+        reshape[1::2] = [self.downscale_factor for i in range(ndims)]
+        
+        # Undo the last view
+        x = x.view(batch_size, channels, *reshape)                                     # (B, C, H, r, W, r)
 
-    after_view = inputs.view(batch_size, channels, *reshape)    # undo the last view
+        # Undo permutation
+        permute = [None] * (2 * ndims)
+        permute[:2 * ndims] = range(3, 2 * ndims + 2, 2)      
+        permute[2 * ndims:] = range(2, 2 * ndims + 1, 2)
+        x = x.permute(0, 1, *permute).contiguous()                                     # (B, C, r, r, H, W)
 
-    # (bs, ch, d1, r, d2, r, ... dN, r) -> (bs, ch, r, ... r, d1, .... dN) , idx: (0, 1, 3, 5, ... 2N + 1, 2, 4, ... 2N)
-    permute = [ None for i in range(2 * N)]
-    permute[:2 * N] = range(3, 2 * N + 2, 2)
-    permute[2 * N:] = range(2, 2 * N + 1, 2)
+        # Undo the first view
+        out_channels = channels * self.downscale_factor ** ndims
+        x = x.view(batch_size, out_channels, *out_dims).contiguous()                   # (B, C * r^N, H, W)
+        return x
 
-    after_shuffle = after_view.permute(0, 1, *permute).contiguous()    # undo permutation
-    return after_shuffle.view(batch_size, out_channels, *out_dims).contiguous()    # undo the first view
+    def extra_repr(self):
+        """ Report downscale factor for a repr. """
+        info = f'downscale_factor={self.downscale_factor}'
+        return info
